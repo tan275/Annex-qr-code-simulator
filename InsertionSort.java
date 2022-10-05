04d93d6137ef21747547c3cbea43e842a6bb9fc3
SilverDragon12 <72190816+SilverDragon12@users.noreply.github.com>
2022-10-05 14:20:29 +0530

    Add files via upload

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,40 +1,42 @@
-import java.util.Scanner;
-
-public class InsertionSort {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int[] a = new int[5];
-        System.out.println("Enter the elements of the array: ");
-        for (int i = 0; i<a.length; i++){
-            a[i] = scan.nextInt();
-        }
-        System.out.println("Array before sorting: ");
-        print(a);
-        long start = System.nanoTime();
-        insertion(a);
-        long end = System.nanoTime();
-        long execution = end - start;
-        double seconds = (double) execution/ 1000000000.0;
-        System.out.println("Array after sorting: ");
-        print(a);
-        System.out.println("Execution time: " + seconds + " seconds");
-
-    }
-    static void print(int[] a){
-        for (int i : a) {
-            System.out.println(i + " ");
-        }
-    }
-    public static void insertion(int[] a){
-        int x, y;
-        for (int i = 1; i<a.length; i++){
-            y = a[i];
-            x = i;
-            while (x>0 && a[x-1]>y){
-                a[x] = a[x-1];
-                x = x-1;
+// Java program for implementation of Insertion Sort
+class InsertionSort {
+    /*Function to sort array using insertion sort*/
+    void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+  
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
             }
-            a[x] = y;
+            arr[j + 1] = key;
         }
     }
-}
+  
+    /* A utility function to print array of size n*/
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+  
+        System.out.println();
+    }
+  
+    // Driver method
+    public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6 };
+  
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+  
+        printArray(arr);
+    }
+} 
