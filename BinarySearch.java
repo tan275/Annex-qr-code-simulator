4a6a60f916ba248e47c60a42c2056010a2b9ae77
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2022-10-06 19:49:30 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -10,6 +10,21 @@ public class BinarySearch {
         int last = arr1.length - 1;
         binarySearch(arr1, 0, last, key);
     }
+//     public static void binarySearch(int[] arr, int first, int last, int key) {
+//         int mid = (first+last)/2;
+//         while(first<=last){
+//             if(arr[mid]==key){
+//                 System.out.println("Element found at index :"+mid);
+//                 break;
+//             }
+//             else if(arr[mid]<key){
+//                 first=mid+1;
+//             }
+//             else{
+//                 last=mid-1;
+//             }
+//             mid=(first+last)/2;
+//         }
     public static void binarySearch(int[] arr, int first, int last, int key) {
         int mid = (first+last)/2;
         while(first<=last){
