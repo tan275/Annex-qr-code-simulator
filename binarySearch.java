ed3b230e681052d4961f6c031c6653257f69f3d3
DeepankRx <deepankpushpad10@gmail.com>
2022-10-09 04:49:49 +0530

    todo app added

--- binarySearch.java
+++ binarySearch.java
@@ -1,32 +1,25 @@
-package com.TrX;
-
-//Program on BinarySearch  #Day1of100DaysOfCoding
-public class BinarySearch {
-
-    public static void main(String[] args) {
-
-        int[] arr1 = {1, 2, 4, 5, 6, 7, 8, 9};
-        int key = 8;
-        int last = arr1.length - 1;
-        binarySearch(arr1, 0, last, key);
-    }
-    public static void binarySearch(int[] arr, int first, int last, int key) {
-        int mid = (first+last)/2;
-        while(first<=last){
-            if(arr[mid]==key){
-                System.out.println("Element found at index :"+mid);
-                break;
-            }
-            else if(arr[mid]<key){
-                first=mid+1;
-            }
-            else{
-                last=mid-1;
-            }
-            mid=(first+last)/2;
-        }
-        if(first>last){
-            System.out.println("Element not Found");
-        }
-    }
-}
+class BinarySearchExample{  
+ public static void binarySearch(int arr[], int first, int last, int key){  
+   int mid = (first + last)/2;  
+   while( first <= last ){  
+      if ( arr[mid] < key ){  
+        first = mid + 1;     
+      }else if ( arr[mid] == key ){  
+        System.out.println("Element is found at index: " + mid);  
+        break;  
+      }else{  
+         last = mid - 1;  
+      }  
+      mid = (first + last)/2;  
+   }  
+   if ( first > last ){  
+      System.out.println("Element is not found!");  
+   }  
+ }  
+ public static void main(String args[]){  
+        int arr[] = {10,20,30,40,50};  
+        int key = 30;  
+        int last=arr.length-1;  
+        binarySearch(arr,0,last,key);     
+ }  
+}  
