f58ba816860ec9536879dde92039cd32182ff5df
Haresh1204 <102413307+Haresh1204@users.noreply.github.com>
2022-10-10 10:56:16 +0530

    Add files via upload

--- Selection.java
+++ Selection.java
@@ -1,52 +1,36 @@
-import java.util.Arrays;
-
 public class Selection {
-    public static void main(String[] args) {
-        int[] arr = {3, 5, 1, 2, 0};
-        reselect(arr,arr.length,0,0);
-        System.out.println(Arrays.toString(arr));
-    }
+    public static void main(String[] args){
+         int i,j,min;
+         int []a=new int[1000];
+        
+           for(i=0;i<1000;i++){
+             a[i]=i;
+          } 
+         long s = System.nanoTime();
+        int swap=0;
+        int comp=0;
+         for(i=0;i<1000;i++){
+            min=i;
+            for(j=i+1;j<1000;j++){
+             
+                 if(a[min]>a[j]){
+                     min=j;
+                    }
+                 comp++;
+             }
+           int temp=a[min];
+           a[min]=a[i];
+           a[i]=temp;
+           swap++;  
+         }
+        long e = System.nanoTime();
+        System.out.println("total time in nano second is: "+(e-s));
 
-    static void select(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int last = arr.length - i - 1;
-            int maxIndex = getMax(arr, 0, last);
-            swap(arr, maxIndex, last);
-        }
-    }
 
-    static int getMax(int[] arr, int start, int end) {
-        int max = start;
-        for (int i = start; i <= end; i++) {
-            if (arr[i] > arr[max]) {
-                max = i;
-            }
-        }
-        return max;
-    }
-
-    static void swap(int[] arr, int maxindex, int last) {
-        int temp = arr[maxindex];
-        arr[maxindex] = arr[last];
-        arr[last] = temp;
-    }
-
-    static void reselect(int[] arr, int r, int c ,int max){
-        if(r==0){
-            return;
-        }
-        if(c<r){
-            if(arr[c]>arr[max]){
-                reselect(arr,r,c+1,c);
-            }else{
-                reselect(arr,r,c+1,max);
-            }
-        }else{
-            int temp=arr[max];
-            arr[max]=arr[r-1];
-            arr[r-1]= temp;
-            reselect(arr,r-1,0,0);
-        }
+     System.out.println("no of swap: "+swap);
+     System.out.println("no of comparison: "+comp);
+          for(i=0;i<1000;i++){
+          System.out.print(+a[i]+",");
+         }
     }
 }
-
